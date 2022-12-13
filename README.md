48e03d168d232b969046cebc642c398558ea02c8
Andrey Didenko <ded14181998@gmail.com>
2022-12-13 20:34:22 +0500

    add makefile, codeclimate

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/AndreyCurious/frontend-project-46/workflows/hexlet-check/badge.svg)](https://github.com/AndreyCurious/frontend-project-46/actions)
 
 <a href="https://asciinema.org/a/YwItbjYomJ0AtpgrsauQqOKXF" target="_blank"><img src="https://asciinema.org/a/YwItbjYomJ0AtpgrsauQqOKXF.svg" /></a>
+
+<a href="https://codeclimate.com/github/AndreyCurious/frontend-project-46/maintainability"><img src="https://api.codeclimate.com/v1/badges/4072cddf8189de6e87d2/maintainability" /></a>
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import gendiff from '../src/index.js'
+import gendiff from '../src/index.js';
+
 const program = new Command();
 
 program
-    .description('Compares two configuration files and shows a difference.')
-    .version('0.0.1')
-    .option('-f, --format <type>', 'output format')
-    .arguments('<filepath1> </filepath2>')
-    .action((filepath1, filepath2) => {console.log(gendiff(filepath1, filepath2))});
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> </filepath2>')
+  .action((filepath1, filepath2) => { console.log(gendiff(filepath1, filepath2)); });
 program.parse();

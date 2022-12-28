99fa2c3963e78d9273cb369d00799f4d678bb57c
Andrey Didenko <ded14181998@gmail.com>
2022-12-28 20:10:07 +0500

    fix 1

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,45 +10,43 @@ const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filen
 
 test.each([
   {
-    file1: getFixturePath('file1.json'),
+    file1: getFixturePath('file1.json'), 
     file2: getFixturePath('file2.json'),
-    expected: readFileSync(getFixturePath('stylishResult'), 'utf-8'),
+    expected: readFileSync(getFixturePath('stylishResult'), 'utf-8')
   },
   {
-    file1: getFixturePath('file1.yml'),
+    file1: getFixturePath('file1.yml'), 
     file2: getFixturePath('file2.yml'),
-    expected: readFileSync(getFixturePath('stylishResult'), 'utf-8'),
+    expected: readFileSync(getFixturePath('stylishResult'), 'utf-8')
   },
   {
-    file1: getFixturePath('file1.yaml'),
+    file1: getFixturePath('file1.yaml'), 
     file2: getFixturePath('file2.yaml'),
-    expected: readFileSync(getFixturePath('stylishResult'), 'utf-8'),
-  },
-])('extensions', ({ file1, file2, expected }) => {
-  expect(gendiff(file1, file2)).toEqual(expected);
-});
+    expected: readFileSync(getFixturePath('stylishResult'), 'utf-8')
+  }
+])('extensions',({file1, file2, expected}) => {
+    expect(gendiff(file1, file2)).toEqual(expected)
+})
 
 test.each([
   {
-    file1: getFixturePath('file1.json'),
+    file1: getFixturePath('file1.json'), 
     file2: getFixturePath('file2.json'),
     format: 'stylish',
-    expected: readFileSync(getFixturePath('stylishResult'), 'utf-8'),
+    expected: readFileSync(getFixturePath('stylishResult'), 'utf-8')
   },
   {
-    file1: getFixturePath('file1.json'),
+    file1: getFixturePath('file1.json'), 
     file2: getFixturePath('file2.json'),
     format: 'json',
-    expected: readFileSync(getFixturePath('jsonResult'), 'utf-8'),
+    expected: readFileSync(getFixturePath('jsonResult'), 'utf-8')
   },
   {
-    file1: getFixturePath('file1.json'),
+    file1: getFixturePath('file1.json'), 
     file2: getFixturePath('file2.json'),
     format: 'plain',
-    expected: readFileSync(getFixturePath('plainResult'), 'utf-8'),
-  },
-])('extensions', ({
-  file1, file2, format, expected,
-}) => {
-  expect(gendiff(file1, file2, format)).toEqual(expected);
-});
+    expected: readFileSync(getFixturePath('plainResult'), 'utf-8')
+  }
+])('extensions',({file1, file2, format, expected}) => {
+    expect(gendiff(file1, file2, format)).toEqual(expected)
+})
